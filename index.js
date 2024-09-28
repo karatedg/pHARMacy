cd0b17624c34dbd6e1b8b6559352a951fe1269d2
karatedg <devan@vargon.com>
2024-09-28 10:38:40 -0400

    interaction detection works in the console

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ async function runImageCheck(event) {
         console.log("Success button click");
         const fileInput = document.getElementById('imageFile');
         const file = fileInput.files[0];
+        let drug1
+        let drugName1
 
         if (file) {
             // Generate a local URL for the uploaded image
@@ -23,11 +25,11 @@ async function runImageCheck(event) {
             document.getElementById('ocrText').textContent = "Processing image...";
             
             // Call your OCR function with the local image URL
-            const drugName = await processOCR(imageUrl);
+            drugName1 = await processOCR(imageUrl);
 
-            if (drugName) {
+            if (drugName1) {
                 console.log("Waiting for drug lookup")
-                const drug1 = await getDrugData(drugName);
+                drug1 = await getDrugData(drugName1);
             } else {
                 console.error('Error processing OCR')
             }
@@ -37,6 +39,8 @@ async function runImageCheck(event) {
 
         const fileInput2 = document.getElementById('imageFile2');
         const file2 = fileInput2.files[0];
+        let drugName2
+        let drug2
 
         if (file2) {
             // Generate a local URL for the uploaded image
@@ -47,11 +51,12 @@ async function runImageCheck(event) {
             document.getElementById('ocrText').textContent = "Processing image...";
             
             // Call your OCR function with the local image URL
-            const drugName2 = await processOCR(imageUrl2);
+            drugName2 = await processOCR(imageUrl2);
 
             if (drugName2) {
                 console.log("Waiting for drug lookup")
-                const drug2 = await getDrugData(drugName2);
+                drug2 = await getDrugData(drugName2);
+                console.log(drug2)
             } else {
                 console.error('Error processing OCR')
             }
@@ -59,8 +64,36 @@ async function runImageCheck(event) {
             alert("Please select a file.");
         }
 
+        console.log("Both drugs have been processed. Ready to compare.");
+        console.log("Drug Name 1: ", drugName1)
+        console.log("Drug 1 Conf: ", drug1)
+        console.log("Drug Name 2: ", drugName2)
+        console.log("Drug 2 Conf: ", drug2)
+
+        let conflict1 = runConflictCheck(drugName1, drug2);
+        let conflict2 = runConflictCheck(drugName2, drug1);
+
 
 }
+
+function runConflictCheck(drugName, conflictsArray) {
+    for (let i = 0; i < conflictsArray.length; i++) {
+        /*
+        console.log("Checking:", conflictsArray[i].term.toLowerCase());
+        console.log("Length:", conflictsArray[i].term.toLowerCase().length);
+        console.log("Against:", drugName);
+        console.log("Length:", drugName.length);
+        */
+        if (drugName.trim() === conflictsArray[i].term.toLowerCase().trim()) {
+            console.log("Conflict detected.");
+            return true;
+        }
+    }
+    console.log("No conflicts detected.");
+    return false;
+}
+
+
 
 async function getDrugData(drug) {
     console.log("Getting drug data...");
@@ -68,12 +101,16 @@ async function getDrugData(drug) {
     console.log('Drug Data:', info);
 
     if (info) {
+        /*
         let ingredientsArray = info.split(',')
         console.log('Parsed Ingredients:', ingredientsArray);
         return ingredientsArray;
+        */
     } else {
         console.error('Error fetching drug data');
     }
+
+    return info
 }
 
 
