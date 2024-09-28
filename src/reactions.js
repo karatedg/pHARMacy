ec8a0ca99e63f7098030e45ae2ce7f0d5313867d
adialyssa <adiawilliams03@gmail.com>
2024-09-28 16:06:23 -0400

    u

--- src/reactions.js
+++ src/reactions.js
@@ -1,13 +1,4 @@
 window.addEventListener("scroll", function () {
-    var header = document.getElementById("header");
-    if (window.scrollY > 0) {
-        header.style.transform = "translateY(-100%)";
-    } else {
-        header.style.transform = "translateY(0)";
-    }
-});
-
-window.addEventListener("scroll", function () {
     var footer = document.getElementById("footer");
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         footer.classList.add("show-footer");
